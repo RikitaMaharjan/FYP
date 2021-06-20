fb3da31ac90dbe0bc5bf4894d2de36e369b2f48a
rikita <rikitamaharjan@gmail.com>
2021-06-20 20:21:09 +0545

    AdminSide

---
+++ app/src/main/java/com/example/ktmeatsserver/Common/Common.java
@@ -0,0 +1,13 @@
+package com.example.ktmeatsserver.Common;
+
+import com.example.ktmeatsserver.Model.User;
+
+public class Common {
+    public static User currentUser;
+
+    public static final String UPDATE = "Update";
+    public static final String DELETE = "Delete";
+
+    public static final int PICK_IMAGE_REQUEST = 71;
+
+}
---
+++ app/src/main/java/com/example/ktmeatsserver/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.ktmeatsserver.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
